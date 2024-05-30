9e0f8c97bff5c964d9d6e8183fe9257c185ee073
alainGonzalez-11 <alain.gonzalez.d@gmail.com>
2024-05-30 11:51:58 -0600

    ✨ Todo list base

--- 03-todo-list/src/components/ToDoList.jsx
+++ 03-todo-list/src/components/ToDoList.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import { useState } from 'react'
 
 const ToDoList = () => {
+  // States are used to save information and are used to update the view automatically
+  const [inputValue, setInputValue] = useState('')
+  const handledAdd = () => {
+    console.log('Add', inputValue)
+  }
   return (
     <>
-      ToDoList
+      <h1>Lista de tareas</h1>
+      {/* Una forma de trabajar con inputs es onChange, la informaci[on estarpa en event.target] */}
+      <input
+        type='text'
+        value={inputValue}
+        onChange={event => setInputValue(event.target.value)}
+      />
+      <button onClick={handledAdd}>Agregar</button>
+      <ul>
+        <li>
+          Item 1<button>Eliminar</button>
+        </li>
+        <li>
+          Item 2<button>Eliminar</button>
+        </li>
+        <li>
+          Item 3<button>Eliminar</button>
+        </li>
+      </ul>
     </>
   )
 }
